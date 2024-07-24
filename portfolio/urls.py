ba5c7b4cae02758bc2a0c0ceabe1193c428a8e78
Gvantsie <g_euashvili@cu.edu.ge>
2024-07-24 18:04:36 +0400

    home rendering

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from portfolio.views import home
 
 urlpatterns = [
-    path("", home, name="home")
+    path("home/", home, name="home")
     ]
--- portfolio_project/urls.py
+++ portfolio_project/urls.py
@@ -22,5 +22,6 @@ from portfolio_project import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('portfolio/', include('portfolio.urls')),
+    path('', include('portfolio.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
